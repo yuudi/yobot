e86e0b72bdd0c96416538878d2bd13473dc279a6
Lancercmd <lancercmd@gmail.com>
2020-07-23 16:16:52 +0800

    feat: confirm with Enter

--- src/client/yobot.py
+++ src/client/yobot.py
@@ -33,8 +33,8 @@ else:
 
 
 class Yobot:
-    Version = "[v3.6.4-beta.1]"
-    Version_id = 202
+    Version = "[v3.6.4-beta.2]"
+    Version_id = 203
     #  "git rev-list --count HEAD"
 
     def __init__(self, *,
