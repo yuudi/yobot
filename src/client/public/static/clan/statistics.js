a7029fdfa17f17da3422201dc171d0bba91748bd
A-kirami <839592615@qq.com>
2020-10-16 09:44:45 +0800

    beautify: unified page style

---
+++ src/client/public/static/clan/statistics.js
@@ -0,0 +1,42 @@
+﻿if (!Object.defineProperty) {
+    alert('浏览器版本过低');
+}
+var vm = new Vue({
+    el: '#app',
+    data: {
+        progressData: [],
+        members: [],
+        tailsData: [],
+        tailsDataVisible: false,
+        group_name: null,
+        reportDate: null,
+        activeIndex: '4',
+        multipleSelection: [],
+        sendRemindVisible: false,
+        send_via_private: false,
+        dropMemberVisible: false,
+        today: 0,
+    },
+    methods: {
+        handleTitleSelect(key, keyPath) {
+            switch (key) {
+                case '1':
+                    window.location = '../';
+                    break;
+                case '2':
+                    window.location = '../subscribers/';
+                    break;
+                case '3':
+                    window.location = '../progress/';
+                    break;
+                case '4':
+                    window.location = '../statistics/';
+                    break;
+                case '5':
+                    window.location = `../my/`;
+                    break;
+            }
+        },
+    }
+})
+
